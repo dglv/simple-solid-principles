66659e905b75d48ba89728280eb907f286c3fb86
dglv <ddem0025@gmail.com>
2024-10-08 00:26:09 +0300

    Fix Dependency Inversion Principle (DIP)

--- src/main/java/org/dglv/solid/MainApp.java
+++ src/main/java/org/dglv/solid/MainApp.java
@@ -3,9 +3,11 @@ package org.dglv.solid;
 import org.dglv.solid.model.Circle;
 import org.dglv.solid.model.Cube;
 import org.dglv.solid.model.Shape;
+import org.dglv.solid.model.Shape3D;
 import org.dglv.solid.model.Square;
 import org.dglv.solid.utils.AreaCalculator;
 import org.dglv.solid.utils.ShapePrinter;
+import org.dglv.solid.utils.VolumeCalculator;
 
 import java.util.List;
 
@@ -17,6 +19,7 @@ public class MainApp {
         Cube cube = new Cube(10);
 
         List<Shape> shapes = List.of(circle, square, cube);
+        List<Shape3D> shapes3d = List.of(cube);
 
         double sum = AreaCalculator.sum(shapes);
         String json = ShapePrinter.json(sum);
@@ -25,5 +28,9 @@ public class MainApp {
         System.out.printf("sum = %.3f\n", sum);
         System.out.println(json);
         System.out.println(csv);
+
+        double sum3d = VolumeCalculator.sum(shapes3d);
+
+        System.out.printf("sum3d = %.3f\n", sum3d);
     }
 }
--- src/main/java/org/dglv/solid/utils/VolumeCalculator.java
+++ src/main/java/org/dglv/solid/utils/VolumeCalculator.java
@@ -1,18 +1,14 @@
 package org.dglv.solid.utils;
 
-import org.dglv.solid.model.Cube;
-import org.dglv.solid.model.Shape;
+import org.dglv.solid.model.Shape3D;
 
 import java.util.List;
 
-// Dependency Inversion Principle (DIP) is broken!
-// Components should depend on abstractions, not on concretions. If another figure than cube appears
-// the calculator won`t be able to process volume of the new figure.
 public record VolumeCalculator () {
 
-    public double sum(List<Cube> cubes) {
-        return cubes.stream()
-                .mapToDouble(Shape::area)
+    public static double sum(List<Shape3D> shapes) {
+        return shapes.stream()
+                .mapToDouble(Shape3D::volume)
                 .sum();
     }
 }
